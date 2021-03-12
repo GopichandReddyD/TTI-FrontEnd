6312435fb740197d6807b1b2c7b78994165e9b6c
GopichandReddyD <gdoggala@ttu.edu>
2021-03-12 21:51:54 +0530

    Adds scroll top down buttons

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -16,6 +16,8 @@ export class LandingPageComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('contactus') ContactUs: ElementRef;
   private subscriptions: Subscription[] = [];
   private scroll$: Subscription;
+  public prevSection: ElementRef;
+  public nextSection: ElementRef;
   public activeSection: ElementRef;
   public activeSectionId: string = 'section-default';
   public HOMEPAGE_SECTIONS = HOME_PAGE_SECTIONS;
@@ -46,25 +48,21 @@ export class LandingPageComponent implements OnInit, AfterViewInit, OnDestroy {
         if (this.activeSectionId !== HOME_PAGE_SECTIONS.DEFAULT) {
           this.scrollTo(this.Home);
         }
-        // this.scrollTo(this.Home);
         break;
       case this.isScrolledIntoView(this.About):
         if (this.activeSectionId !== HOME_PAGE_SECTIONS.ABOUT) {
           this.scrollTo(this.About);
         }
-        // this.scrollTo(this.About);
         break;
       case this.isScrolledIntoView(this.Contributor):
         if (this.activeSectionId !== HOME_PAGE_SECTIONS.CONTRIBUTOR) {
           this.scrollTo(this.Contributor);
         }
-        // this.scrollTo(this.Contributor);
         break;
       case this.isScrolledIntoView(this.ContactUs):
         if (this.activeSectionId !== HOME_PAGE_SECTIONS.CONTACTUS) {
           this.scrollTo(this.ContactUs);
         }
-        // this.scrollTo(this.ContactUs);
         break;
     }
   }
@@ -80,16 +78,24 @@ export class LandingPageComponent implements OnInit, AfterViewInit, OnDestroy {
   private getElementRef(selectionId: string) {
     switch (selectionId) {
       case HOME_PAGE_SECTIONS.DEFAULT:
+        this.prevSection = null;
         this.activeSection = this.Home;
+        this.nextSection = this.About;
         break;
       case HOME_PAGE_SECTIONS.ABOUT:
+        this.prevSection = this.Home
         this.activeSection = this.About;
+        this.nextSection = this.Contributor;
         break;
       case HOME_PAGE_SECTIONS.CONTRIBUTOR:
+        this.prevSection = this.About
         this.activeSection = this.Contributor;
+        this.nextSection = this.ContactUs
         break;
       case HOME_PAGE_SECTIONS.CONTACTUS:
+        this.prevSection = this.Contributor
         this.activeSection = this.ContactUs;
+        this.nextSection = null;
         break;
       default: HOME_PAGE_SECTIONS.DEFAULT;
     }
@@ -104,8 +110,8 @@ export class LandingPageComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   public scrollTo(el: ElementRef) {
-    this.activeSection = el;
     this.activeSectionId = el.nativeElement.id;
+    this.getElementRef(this.activeSectionId)
     this.updateTransparentHeader();
     this.activeSection.nativeElement.scrollIntoView({
       behavior: 'smooth'
